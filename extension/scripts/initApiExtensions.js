77f4e9d77bc011cd79fc1f0f0b9d70f4babaeab6
Lam Tran <lam.tran@commercetools.de>
2019-02-19 16:24:44 +0100

    #1 simplify creation of api extension

--- extension/scripts/initApiExtensions.js
+++ extension/scripts/initApiExtensions.js
@@ -1,17 +1,14 @@
-const Promise = require('bluebird')
 const _ = require('lodash')
 
-const apiExtensionDrafts = require('../resources/api-extensions.json')
+const apiExtensionDraft = require('../resources/api-extension.json')
 
 async function initApiExtensions (ctpClient, ctpAdyenIntegrationBaseUrl) {
-  await Promise.map(apiExtensionDrafts.values(), async (uncompiledDraft) => {
-    try {
-      const extension = _.template(JSON.stringify(uncompiledDraft))({ ctpAdyenIntegrationBaseUrl })
-      await ctpClient.create(ctpClient.builder.extensions, extension)
-    } catch (e) {
-      console.error('Error when creating interface interaction custom type, skipping...', JSON.stringify(e))
-    }
-  }, { concurrency: 3 })
+  try {
+    const extension = _.template(JSON.stringify(apiExtensionDraft))({ ctpAdyenIntegrationBaseUrl })
+    await ctpClient.create(ctpClient.builder.extensions, extension)
+  } catch (e) {
+    console.error('Error when creating interface interaction custom type, skipping...', JSON.stringify(e))
+  }
 }
 
 module.exports = {
