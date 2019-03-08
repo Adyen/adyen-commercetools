4cde4bd5923c9151ff7f8500f445e119218d73fd
Lam Tran <lam.tran@commercetools.de>
2019-03-08 12:02:15 +0100

    #14 update paths in cypress files

--- cypress/setUp/credit_card_3d_redirect.js
+++ cypress/setUp/credit_card_3d_redirect.js
@@ -6,7 +6,7 @@ global.navigator = {}
 
 const adyenEncrypt = require('adyen-cse-js')
 const _ = require('lodash')
-const ctpClientBuilder = require('../../extension/src/ctp/ctpClient')
+const ctpClientBuilder = require('../../extension/src/ctp/ctp-client')
 const payment3dTemplate = require('../../extension/test/fixtures/payment-credit-card-3d.json')
 
 const key = process.env.CLIENT_ENCRYPTION_PUBLIC_KEY
--- cypress/setUp/payment_resources.js
+++ cypress/setUp/payment_resources.js
@@ -1,9 +1,9 @@
 // Prepare all CTP resources that are needed to execute Adyen payments
 // like custom types, API integration
 
-const { initResources } = require('../../extension/src/config/init/initResources')
+const { initResources } = require('../../extension/src/config/init/init-resources')
 const testUtils = require('../../extension/test/test-utils')
-const ctpClientBuilder = require('../../extension/src/ctp/ctpClient')
+const ctpClientBuilder = require('../../extension/src/ctp/ctp-client')
 
 async function init () {
   const ctpClient = ctpClientBuilder.get()
