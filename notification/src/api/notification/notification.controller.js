d9ed986cc744a0e681d5896070d4f621e3b7e096
leqada <leqada@gmail.com>
2019-03-11 01:35:42 +0100

    minor refactoring of imports

--- notification/src/api/notification/notification.controller.js
+++ notification/src/api/notification/notification.controller.js
@@ -1,10 +1,10 @@
 const _ = require('lodash')
 const httpUtils = require('../../utils/commons')
-const config = require('../../config/config').load()
 const ctp = require('../../utils/ctp')
-const ctpClient = ctp.get(config)
 const { processNotifications } = require('../../handler/notification/notification.handler')
+const config = require('../../config/config').load()
 
+const ctpClient = ctp.get(config)
 
 // TODO: add JSON schema validation:
 // https://github.com/commercetools/commercetools-adyen-integration/issues/9
