2fc25f640f8e57f60ce72faf680463059a37bea0
Hasan Mammed-zadeh <leqada@gmail.com>
2019-03-25 16:59:12 +0100

    create notification interfaceInteraction type on server start

--- notification/src/init.js
+++ notification/src/init.js
@@ -1,7 +1,11 @@
 const server = require('./server.js')
+const { ensureInterfaceInteractionCustomType } = require('./config/init/ensure-interface-interaction-custom-type')
+const ctp = require('./utils/ctp')
+const ctpClient = ctp.get(config)
 
 const PORT = process.env.PORT || 443
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+  await ensureInterfaceInteractionCustomType(ctpClient)
   console.log(`Server running at http://127.0.0.1:${PORT}/`)
 })
