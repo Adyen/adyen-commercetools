6775dd7726b86b381d0d12eb945751aad0defe39
Hasan Mammed-zadeh <leqada@gmail.com>
2019-03-27 13:53:45 +0100

    fix initialization

--- notification/src/init.js
+++ notification/src/init.js
@@ -1,6 +1,7 @@
-const server = require('./server.js')
+const server = require('./server.js').setupServer()
 const { ensureInterfaceInteractionCustomType } = require('./config/init/ensure-interface-interaction-custom-type')
 const ctp = require('./utils/ctp')
+const config = require('./config/config')()
 const ctpClient = ctp.get(config)
 
 const PORT = process.env.PORT || 443
