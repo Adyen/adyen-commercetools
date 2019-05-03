447905fcde8e01a39fc6685f3459c772c7a2f8c5
Lam Tran <lam.tran@commercetools.de>
2019-05-03 12:06:27 +0200

    Add missing require config

--- extension/src/init.js
+++ extension/src/init.js
@@ -1,14 +1,17 @@
-const init = require('./server.js').setupServer()
+const server = require('./server.js').setupServer()
 const utils = require('./utils')
 
 const { ensureResources } = require('./config/init/ensure-resources')
+const configLoader = require('./config/config')
+
+const config = configLoader.load()
 
 const port = parseInt(process.env.EXTENSION_PORT || 8080, 10)
 const logger = utils.getLogger()
 
 if (config.keepAliveTimeout !== undefined)
   server.keepAliveTimeout = config.keepAliveTimeout
-init.listen(port, async () => {
+server.listen(port, async () => {
   await ensureResources()
   logger.info(`Server running at http://127.0.0.1:${port}/`)
 })
