d580dffa78388699752cd11323fc8fe38964add0
Lam Tran <lam.tran@commercetools.de>
2020-04-01 17:31:50 +0200

    #230 remove catching errors on ensuring resources

--- extension/src/config/init/ensure-interface-interaction-custom-type.js
+++ extension/src/config/init/ensure-interface-interaction-custom-type.js
@@ -5,16 +5,11 @@ const interfaceInteractionType = require('../../../resources/payment-interface-i
 const logger = utils.getLogger()
 
 async function ensureInterfaceInteractionCustomType (ctpClient) {
-  try {
-    const { body } = await ctpClient.fetch(ctpClient.builder.types.where(`key="${interfaceInteractionType.key}"`))
-    if (body.results.length === 0) {
-      await ctpClient.create(ctpClient.builder.types, interfaceInteractionType)
-      logger.info('Successfully created a payment interface interaction'
-        + `type (key=${interfaceInteractionType.key})`)
-    }
-  } catch (e) {
-    logger.error(e, `Error when creating payment interface interaction type (key=${interfaceInteractionType.key}), `
-     + 'skipping...')
+  const { body } = await ctpClient.fetch(ctpClient.builder.types.where(`key="${interfaceInteractionType.key}"`))
+  if (body.results.length === 0) {
+    await ctpClient.create(ctpClient.builder.types, interfaceInteractionType)
+    logger.info('Successfully created a payment interface interaction'
+      + `type (key=${interfaceInteractionType.key})`)
   }
 }
 
--- extension/src/config/init/ensure-payment-custom-type.js
+++ extension/src/config/init/ensure-payment-custom-type.js
@@ -5,15 +5,10 @@ const utils = require('../../utils')
 const logger = utils.getLogger()
 
 async function ensurePaymentCustomType (ctpClient) {
-  try {
-    const { body } = await ctpClient.fetch(ctpClient.builder.types.where(`key="${paymentCustomType.key}"`))
-    if (body.results.length === 0) {
-      await ctpClient.create(ctpClient.builder.types, paymentCustomType)
-      logger.info(`Successfully created a payment type (key=${paymentCustomType.key})`)
-    }
-  } catch (e) {
-    logger.error(e, `Error when creating payment type (key=${paymentCustomType.key}),  `
-      + 'skipping...')
+  const { body } = await ctpClient.fetch(ctpClient.builder.types.where(`key="${paymentCustomType.key}"`))
+  if (body.results.length === 0) {
+    await ctpClient.create(ctpClient.builder.types, paymentCustomType)
+    logger.info(`Successfully created a payment type (key=${paymentCustomType.key})`)
   }
 }
 
