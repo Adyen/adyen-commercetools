f9a18d6fa22118313447f5ad6a9b3ded27ebc44a
aoz <bilmuhahmet@gmail.com>
2020-04-01 09:07:34 +0200

    update default adyen base url to v53.

--- extension/src/config/config.js
+++ extension/src/config/config.js
@@ -25,8 +25,8 @@ function getAdyenCredentials () {
   return {
     merchantAccount: process.env.ADYEN_MERCHANT_ACCOUNT,
     apiKey: process.env.ADYEN_API_KEY,
-    apiBaseUrl: process.env.ADYEN_API_BASE_URL || 'https://checkout-test.adyen.com/v40',
-    legacyApiBaseUrl: process.env.ADYEN_LEGACY_API_BASE_URL || 'https://pal-test.adyen.com/pal/servlet/Payment/v40'
+    apiBaseUrl: process.env.ADYEN_API_BASE_URL || 'https://checkout-test.adyen.com/v53',
+    legacyApiBaseUrl: process.env.ADYEN_LEGACY_API_BASE_URL || 'https://pal-test.adyen.com/pal/servlet/Payment/v53'
   }
 }
 
