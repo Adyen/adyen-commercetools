a48be471fd6b07f2f29f90297e6cc16a7038a94b
aoz <bilmuhahmet@gmail.com>
2020-04-07 16:18:53 +0200

    add unit test

---
+++ extension/test/unit/get-payment-methods.handler.spec.js
@@ -0,0 +1,102 @@
+const { expect } = require('chai')
+const sinon = require('sinon')
+const fetch = require('node-fetch')
+const _ = require('lodash')
+
+const c = require('../../src/config/constants')
+const { handlePayment } = require('../../src/paymentHandler/get-payment-methods.handler')
+
+describe('get-payment-methods::handlePayment::', () => {
+  const getPaymentMethodsRequest = {
+    countryCode: "DE",
+    shopperLocale: "de-DE",
+    amount: {
+      currency: "EUR",
+      value: 1000
+    }
+  }
+  const paymentObject = {
+    amountPlanned: {
+      currencyCode: 'EUR',
+      centAmount: 1000,
+    },
+    paymentMethodInfo: {
+      paymentInterface: c.CTP_ADYEN_INTEGRATION
+    },
+    interfaceInteractions: [],
+    custom: {
+      type: {
+        typeId: "type",
+        key: c.CTP_WEB_COMPONENTS_PAYMENT_TYPE_KEY
+      },
+      fields: {
+        getPaymentMethodsRequest: JSON.stringify(getPaymentMethodsRequest)
+      }
+    }
+  }
+
+  it('handlePayment should return the right actions', async () => {
+    const adyenGetPaymentResponse = {
+      "groups": [
+        {
+          "name": "Credit Card",
+          "types": [
+            "visa",
+            "mc"
+          ]
+        },
+      ],
+      "paymentMethods": [
+        {
+          "name": "PayPal",
+          "supportsRecurring": true,
+          "type": "paypal"
+        }
+      ]
+    }
+    const fetchStub = sinon.stub(fetch, 'Promise')
+      .resolves({ json: () => (adyenGetPaymentResponse)})
+
+    const result = await handlePayment(paymentObject)
+
+    expect(result.actions.length).to.equal(2)
+    expect(result.actions[0].action).to.equal('addInterfaceInteraction')
+    expect(result.actions[1].action).to.equal('setCustomField')
+    expect(JSON.parse(result.actions[0].fields.request)).to.be.deep.includes(getPaymentMethodsRequest)
+    expect(result.actions[0].fields.response).to.be.deep.equal(JSON.stringify(adyenGetPaymentResponse))
+    expect(result.actions[0].fields.response).to.be.deep.equal(result.actions[1].value)
+    expect(result.actions[0].fields.type).to.equal(c.CTP_INTERACTION_TYPE_GET_PAYMENT_METHODS)
+    expect(result.actions[1].name).to.equal(c.CTP_CUSTOM_FIELD_GET_PAYMENT_METHODS_RESPONSE)
+    fetchStub.restore()
+  })
+
+  it('when adyen request fails ' +
+    'then handlePayment should return the right actions with failed responses', async () => {
+    const errorMsg = "unexpected exception"
+    const fetchStub = sinon.stub(fetch, 'Promise').rejects(errorMsg)
+
+    const result = await handlePayment(paymentObject)
+
+    expect(result.actions.length).to.equal(2)
+    expect(result.actions[0].action).to.equal('addInterfaceInteraction')
+    expect(result.actions[1].action).to.equal('setCustomField')
+    expect(JSON.parse(result.actions[0].fields.request)).to.be.deep.includes(getPaymentMethodsRequest)
+    expect(result.actions[0].fields.response).to.be.includes(errorMsg)
+    expect(result.actions[0].fields.type).to.equal(c.CTP_INTERACTION_TYPE_GET_PAYMENT_METHODS)
+    expect(result.actions[1].name).to.equal(c.CTP_CUSTOM_FIELD_GET_PAYMENT_METHODS_RESPONSE)
+    fetchStub.restore()
+  })
+
+  //todo(ahmet): could not decide where to put those checks, looks it should not be part of the handlePayment.
+  it.skip('when getPaymentMethodsRequest custom field is not a valid json object ' +
+    'then handlePayment should return the right actions with failed responses', async () => {
+    const _paymentObject = _.cloneDeep(paymentObject)
+    _paymentObject.custom.fields.getPaymentMethodsRequest= "{"
+
+    const result = await handlePayment(_paymentObject)
+    expect(result.actions.length).to.equal(2)
+    expect(result.actions[0].action).to.equal('addInterfaceInteraction')
+    expect(result.actions[1].action).to.equal('setCustomField')
+    expect(result.actions[0].fields.request).to.be.includes('Unexpected end of JSON input')
+  })
+})
