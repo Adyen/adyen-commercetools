cf908e35b46a6f5dc76299507004e9959fab1fac
aoz <bilmuhahmet@gmail.com>
2020-05-08 17:39:55 +0200

    add a todo for additionalData removal.

--- notification/src/handler/notification/notification.handler.js
+++ notification/src/handler/notification/notification.handler.js
@@ -95,6 +95,7 @@ function calculateUpdateActionsForPayment (payment, notification) {
 }
 
 function getAddInterfaceInteractionUpdateAction (notification) {
+  // TODO(ahmetoz): clarify https://github.com/commercetools/commercetools-adyen-integration/issues/260
   // strip away sensitive data
   delete notification.additionalData
   delete notification.reason
