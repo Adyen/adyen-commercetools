418d750d3d38a67a4428b7fc2094207e84f09996
Lam Tran <lam.tran@commercetools.de>
2020-05-11 11:57:19 +0200

    #279 add validation for submit additional details request

--- extension/src/validator/error-messages.js
+++ extension/src/validator/error-messages.js
@@ -13,5 +13,7 @@ module.exports = {
   MISSING_MD: 'Set custom.fields.MD',
   GET_ORIGIN_KEYS_REQUEST_INVALID_JSON: 'getOriginKeysRequest does not contain valid JSON.',
   GET_PAYMENT_METHODS_REQUEST_INVALID_JSON: 'getOriginKeysRequest does not contain valid JSON.',
-  MAKE_PAYMENT_REQUEST_INVALID_JSON: 'makePaymentRequest does not contain valid JSON.'
+  MAKE_PAYMENT_REQUEST_INVALID_JSON: 'makePaymentRequest does not contain valid JSON.',
+  // eslint-disable-next-line max-len
+  SUBMIT_ADDITIONAL_PAYMENT_DETAILS_REQUEST_INVALID_JSON: 'submitAdditionalPaymentDetailsRequest does not contain valid JSON.'
 }
--- extension/test/unit/validator-builder.spec.js
+++ extension/test/unit/validator-builder.spec.js
@@ -2,6 +2,12 @@ const { expect } = require('chai')
 const async = require('async')
 
 const ValidatorBuilder = require('../../src/validator/validator-builder')
+const {
+  GET_ORIGIN_KEYS_REQUEST_INVALID_JSON,
+  GET_PAYMENT_METHODS_REQUEST_INVALID_JSON,
+  MAKE_PAYMENT_REQUEST_INVALID_JSON,
+  SUBMIT_ADDITIONAL_PAYMENT_DETAILS_REQUEST_INVALID_JSON
+} = require('../../src/validator/error-messages')
 
 describe('Validator builder', () => {
   const vb = ValidatorBuilder.withPayment({})
@@ -22,4 +28,25 @@ describe('Validator builder', () => {
       })
     }
   )
+
+  it('on invalid JSON validateRequestFields() should return error object', async () => {
+    const invalidPaymentString = {
+      custom: {
+        fields: {
+          getOriginKeysRequest: '{"a"}',
+          getPaymentMethodsRequest: '{"a"}',
+          makePaymentRequest: '{"a"}',
+          submitAdditionalPaymentDetailsRequest: '{"a"}'
+        }
+      }
+    }
+    const errorObject = ValidatorBuilder.withPayment(invalidPaymentString)
+      .validateRequestFields()
+      .getErrors()
+    expect(errorObject.getOriginKeysRequest).to.equal(GET_ORIGIN_KEYS_REQUEST_INVALID_JSON)
+    expect(errorObject.getPaymentMethodsRequest).to.equal(GET_PAYMENT_METHODS_REQUEST_INVALID_JSON)
+    expect(errorObject.makePaymentRequest).to.equal(MAKE_PAYMENT_REQUEST_INVALID_JSON)
+    expect(errorObject.submitAdditionalPaymentDetailsRequest)
+      .to.equal(SUBMIT_ADDITIONAL_PAYMENT_DETAILS_REQUEST_INVALID_JSON)
+  })
 })
