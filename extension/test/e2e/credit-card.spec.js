7c1e33764c64dd100721140ab7b2e6810e52b957
Lam Tran <lam.tran@commercetools.de>
2020-06-03 15:05:37 +0200

    #298 fix tests

--- extension/test/e2e/credit-card.spec.js
+++ extension/test/e2e/credit-card.spec.js
@@ -1,12 +1,10 @@
 const puppeteer = require('puppeteer')
 const nodeStatic = require('node-static')
-const itParam = require('mocha-param')
 const { expect } = require('chai')
 const iTSetUp = require('../integration/integration-test-set-up')
 const ctpClientBuilder = require('../../src/ctp/ctp-client')
 const { routes } = require('../../src/routes')
 const c = require('../../src/config/constants')
-
 
 describe('credit-card-payment', () => {
   let browser
@@ -34,7 +32,10 @@ describe('credit-card-payment', () => {
     { name: 'V Pay', creditCardNumber: '4013 2500 0000 0000 006' },
   ]
 
-  before(async () => {
+  // note: ngrok should be restarted for every test case, otherwise there will be
+  // 429 Too Many Requests error. This is due to the limit of maximum opened HTTP connections,
+  // which is 40 connections at the same time.
+  beforeEach(async () => {
     const fileServer = new nodeStatic.Server()
     routes['/make-payment-form'] = async (request, response) => {
       fileServer.serveFile('./test/e2e/fixtures/make-payment-form.html', 200, {}, request, response)
@@ -48,76 +49,83 @@ describe('credit-card-payment', () => {
     })
   })
 
-  after(async () => {
+  afterEach(async () => {
     await iTSetUp.cleanupResources()
     await browser.close()
   })
 
-  // eslint-disable-next-line no-template-curly-in-string
-  itParam('when issuer is ${value.name} and number is ${value.creditCardNumber}, ' +
-    'it should successfully finish the payment with credit card',
-    creditCards,
-    async ({ creditCardNumber, creditCardDate = '03/30', creditCardCvc = '737' }) => {
-      const page = await browser.newPage()
-      const baseUrl = process.env.API_EXTENSION_BASE_URL
+  creditCards.forEach(({
+                         name, creditCardNumber, creditCardDate = '03/30', creditCardCvc = '737'
+                       }) => {
+    // eslint-disable-next-line no-template-curly-in-string
+    it(`when issuer is ${name} and number is ${creditCardNumber}, ` +
+      'it should successfully finish the payment with credit card',
+      async () => {
+        const page = await browser.newPage()
+        const baseUrl = process.env.API_EXTENSION_BASE_URL
 
-      const getOriginKeysRequestDraft = {
-        originDomains: [
-          baseUrl
-        ]
-      }
-      const paymentDraft = {
-        amountPlanned: {
-          currencyCode: 'EUR',
-          centAmount: 1000,
-        },
-        paymentMethodInfo: {
-          paymentInterface: c.CTP_ADYEN_INTEGRATION
-        },
-        custom: {
-          type: {
-            typeId: 'type',
-            key: c.CTP_PAYMENT_CUSTOM_TYPE_KEY
+        const getOriginKeysRequestDraft = {
+          originDomains: [
+            baseUrl
+          ]
+        }
+        const paymentDraft = {
+          amountPlanned: {
+            currencyCode: 'EUR',
+            centAmount: 1000,
           },
-          fields: {
-            getOriginKeysRequest: JSON.stringify(getOriginKeysRequestDraft)
+          paymentMethodInfo: {
+            paymentInterface: c.CTP_ADYEN_INTEGRATION
+          },
+          custom: {
+            type: {
+              typeId: 'type',
+              key: c.CTP_PAYMENT_CUSTOM_TYPE_KEY
+            },
+            fields: {
+              getOriginKeysRequest: JSON.stringify(getOriginKeysRequestDraft)
+            }
           }
         }
-      }
 
-      const { body: payment } = await ctpClient.create(ctpClient.builder.payments, paymentDraft)
-      const { getOriginKeysResponse: getOriginKeysResponseString } = payment.custom.fields
-      const getOriginKeysResponse = await JSON.parse(getOriginKeysResponseString)
+        const { body: payment } = await ctpClient.create(ctpClient.builder.payments, paymentDraft)
+        const { getOriginKeysResponse: getOriginKeysResponseString } = payment.custom.fields
+        const getOriginKeysResponse = await JSON.parse(getOriginKeysResponseString)
 
-      await page.goto(`${baseUrl}/make-payment-form`)
-      await page.type('#adyen-origin-key', getOriginKeysResponse.originKeys[baseUrl])
-      await page.$eval('#adyen-origin-key', e => e.blur())
-      await page.waitFor(2000)
-      await executeInAdyenIframe(page, '#encryptedCardNumber', el => el.type(creditCardNumber))
-      await executeInAdyenIframe(page, '#encryptedExpiryDate', el => el.type(creditCardDate))
-      await executeInAdyenIframe(page, '#encryptedSecurityCode', el => el.type(creditCardCvc))
-      await page.click('.adyen-checkout__button--pay')
-      const makePaymentRequestTextArea = await page.$('#adyen-make-payment-request')
-      const makePaymentRequest = await (await makePaymentRequestTextArea.getProperty('innerHTML')).jsonValue()
-      const { body: updatedPayment } = await ctpClient.update(ctpClient.builder.payments, payment.id, payment.version, [
-        { action: 'setCustomField', name: 'makePaymentRequest', value: makePaymentRequest }
-      ])
+        await page.goto(`${baseUrl}/make-payment-form`)
+        await page.type('#adyen-origin-key', getOriginKeysResponse.originKeys[baseUrl])
+        await page.$eval('#adyen-origin-key', e => e.blur())
+        await page.waitFor(2000)
+        await executeInAdyenIframe(page, '#encryptedCardNumber', el => el.type(creditCardNumber))
+        await executeInAdyenIframe(page, '#encryptedExpiryDate', el => el.type(creditCardDate))
+        await executeInAdyenIframe(page, '#encryptedSecurityCode', el => el.type(creditCardCvc))
+        await page.click('.adyen-checkout__button--pay')
+        const makePaymentRequestTextArea = await page.$('#adyen-make-payment-request')
+        const makePaymentRequest = await (await makePaymentRequestTextArea.getProperty('innerHTML')).jsonValue()
+        const { body: updatedPayment } = await ctpClient.update(ctpClient.builder.payments, payment.id,
+          payment.version, [{
+            action: 'setCustomField',
+            name: 'makePaymentRequest',
+            value: makePaymentRequest
+          }])
 
-      const { makePaymentResponse: makePaymentResponseString } = updatedPayment.custom.fields
-      const makePaymentResponse = await JSON.parse(makePaymentResponseString)
-      expect(makePaymentResponse.resultCode).to.equal('Authorised',
-        `resultCode is not Authorised: ${makePaymentResponseString}`)
-      expect(makePaymentResponse.pspReference).to.match(/[A-Z0-9]+/,
-        `pspReference does not match '/[A-Z0-9]+/': ${ makePaymentResponseString}`)
+        const { makePaymentResponse: makePaymentResponseString } = updatedPayment.custom.fields
+        const makePaymentResponse = await JSON.parse(makePaymentResponseString)
+        expect(makePaymentResponse.resultCode).to.equal('Authorised',
+          `resultCode is not Authorised: ${makePaymentResponseString}`)
+        expect(makePaymentResponse.pspReference).to.match(/[A-Z0-9]+/,
+          `pspReference does not match '/[A-Z0-9]+/': ${makePaymentResponseString}`)
 
-      expect(updatedPayment.transactions).to.have.lengthOf(1)
-      const transaction = updatedPayment.transactions[0]
-      expect(transaction.state).to.equal(c.CTP_TXN_STATE_SUCCESS)
-      expect(transaction.type).to.equal('Authorization')
-      expect(transaction.interactionId).to.equal(makePaymentResponse.pspReference)
-      expect(transaction.amount.centAmount).to.equal(updatedPayment.amountPlanned.centAmount)
-      expect(transaction.amount.currencyCode).to.equal(updatedPayment.amountPlanned.currencyCode)
-    })
+        expect(updatedPayment.transactions).to.have.lengthOf(1)
+        const transaction = updatedPayment.transactions[0]
+        expect(transaction.state).to.equal(c.CTP_TXN_STATE_SUCCESS)
+        expect(transaction.type).to.equal('Authorization')
+        expect(transaction.interactionId).to.equal(makePaymentResponse.pspReference)
+        expect(transaction.amount.centAmount).to.equal(updatedPayment.amountPlanned.centAmount)
+        expect(transaction.amount.currencyCode).to.equal(updatedPayment.amountPlanned.currencyCode)
+      })
+  })
+
 
   async function executeInAdyenIframe (page, selector, executeFn) {
     for (const frame of page.mainFrame().childFrames()) {
