a89ff0cf8541dd8f73eb0792d017025d0a75d54e
aoz <bilmuhahmet@gmail.com>
2020-06-10 13:44:03 +0200

    add json validation

--- extension/src/validator/error-messages.js
+++ extension/src/validator/error-messages.js
@@ -16,5 +16,6 @@ module.exports = {
   MAKE_PAYMENT_REQUEST_INVALID_JSON: 'makePaymentRequest does not contain valid JSON.',
   // eslint-disable-next-line max-len
   SUBMIT_ADDITIONAL_PAYMENT_DETAILS_REQUEST_INVALID_JSON: 'submitAdditionalPaymentDetailsRequest does not contain valid JSON.',
-  AMOUNT_PLANNED_CHANGE_NOT_ALLOWED: 'amountPlanned field cannot be changed after makePayment request is made to Adyen'
+  AMOUNT_PLANNED_CHANGE_NOT_ALLOWED: 'amountPlanned field cannot be changed after makePayment request is made to Adyen',
+  MANUAL_CAPTURE_REQUEST_INVALID_JSON: 'manualCaptureRequest does not contain valid JSON.',
 }
