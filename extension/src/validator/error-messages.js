614010eb57b9156e19e8dcb575e19c32244bdd4a
Lam Tran <lam.tran@commercetools.de>
2020-10-29 10:42:06 +0100

    #461 update error message

--- extension/src/validator/error-messages.js
+++ extension/src/validator/error-messages.js
@@ -4,6 +4,7 @@ module.exports = {
   MAKE_PAYMENT_REQUEST_INVALID_JSON: 'makePaymentRequest does not contain valid JSON.',
   // eslint-disable-next-line max-len
   SUBMIT_ADDITIONAL_PAYMENT_DETAILS_REQUEST_INVALID_JSON: 'submitAdditionalPaymentDetailsRequest does not contain valid JSON.',
-  AMOUNT_PLANNED_NOT_SAME: 'amountPlanned field must be the same as the amount in makePayment request',
+  AMOUNT_PLANNED_NOT_SAME: 'amountPlanned field must be the same as the amount in '
+    + 'makePayment request in the interface interactions or in the custom field',
   MAKE_PAYMENT_REQUEST_MISSING_REFERENCE: 'Required "reference" field is missing in makePaymentRequest.'
 }
