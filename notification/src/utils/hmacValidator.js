9cbca73815a643dad71d3cd77e0f0352cb27d5fc
Lam Tran <lam.tran@commercetools.de>
2020-10-29 21:16:05 +0100

    #407 Improve error logging for hmac validations (#465)

--- notification/src/utils/hmacValidator.js
+++ notification/src/utils/hmacValidator.js
@@ -4,7 +4,7 @@ const config = require('../config/config')()
 /* eslint-disable new-cap */
 const validator = new hmacValidator()
 
-function hasValidHmacSignature (notification) {
+function validateHmacSignature (notification) {
   /* By verifying this (hmacSignature) signature, We confirm that the notification was sent by Adyen,
   and was not modified during transmission.
   A sample representation will look like:
@@ -18,10 +18,19 @@ function hasValidHmacSignature (notification) {
   }
   */
   const notificationRequestItem = notification.NotificationRequestItem
-  if (!notificationRequestItem.additionalData || !notificationRequestItem.additionalData.hmacSignature)
-    return false
-
-  return validator.validateHMAC(notificationRequestItem, config.adyen.secretHmacKey)
+  if (!notificationRequestItem.additionalData)
+    return 'Notification does not contain the required field '
+      + '"NotificationRequestItem.additionalData". Please check if HMAC is configured correctly or contact Adyen.'
+  if (!notificationRequestItem.additionalData.hmacSignature)
+    return 'Notification does not contain the required field '
+      + '"NotificationRequestItem.additionalData.hmacSignature". '
+      + 'Please check if HMAC is configured correctly or contact Adyen.'
+  const validationResult = validator.validateHMAC(notificationRequestItem, config.adyen.secretHmacKey)
+  if (!validationResult)
+    return 'Notification does not have a valid HMAC signature, '
+      + 'please confirm that the notification was sent by Adyen, '
+      + 'and was not modified during transmission.'
+  return null
 }
 
-module.exports = { hasValidHmacSignature }
+module.exports = { validateHmacSignature }
