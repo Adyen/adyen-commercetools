e68c093c4fcb2fe59f5d25df6075313194e13346
jude.niroshan11@gmail.com <jude.niroshan11@gmail.com>
2021-01-25 09:29:45 +0100

    handle exceptions in notification cloud function

--- notification/index.js
+++ notification/index.js
@@ -7,12 +7,33 @@
  * Entry point: notificationTrigger
  */
 
-const notificationController = require('./src/api/notification/notification.controller')
-const httpUtils = require('./src/utils/commons')
+const ctp = require('./src/utils/ctp')
+const handler = require('./src/handler/notification/notification.handler')
+const config = require('./src/config/config')()
+const logger = require('./src/utils/logger').getLogger()
+const { getNotificationForTracking } = require('./src/utils/commons')
 
-httpUtils.collectRequestData = (request) => JSON.stringify(request.body)
-exports.httpUtils = httpUtils
+const ctpClient = ctp.get(config)
 
 exports.notificationTrigger = async (request, response) => {
-  await notificationController.handleNotification(request, response)
+  try {
+    await handler.processNotifications(
+      request.body.notificationItems,
+      ctpClient
+    )
+    response.status(200).send({
+      notificationResponse: '[accepted]',
+    })
+  } catch (e) {
+    logger.error(
+      {
+        notification: getNotificationForTracking(
+          request.body.notificationItems
+        ),
+        err: e,
+      },
+      'Unexpected error when processing event'
+    )
+    throw e
+  }
 }
