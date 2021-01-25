58bcf35a1af9be43c11b040ea8c52a86c54419ec
lojzatran <lam.tran@commercetools.com>
2021-01-25 11:46:45 +0100

    fix unit test for multitenancy project selection

--- extension/test/unit/multitenancy.spec.js
+++ extension/test/unit/multitenancy.spec.js
@@ -4,44 +4,6 @@ const nock = require('nock')
 const ctpPayment = require('./fixtures/ctp-payment.json')
 const ctpCart = require('./fixtures/ctp-cart')
 const paymentSuccessResponse = require('./fixtures/adyen-make-payment-success-response')
-
-// To override the config,
-// the overriding must be done before all `require` that loads config.js file
-process.env.ADYEN_INTEGRATION_CONFIG = JSON.stringify({
-  commercetools: {
-    ctpProjectKey1: {
-      clientId: 'clientId',
-      clientSecret: 'clientSecret',
-      ensureResources: true
-    },
-    ctpProjectKey2: {
-      clientId: 'clientId2',
-      clientSecret: 'clientSecret2',
-      ensureResources: true
-    },
-    ctpProjectKey3: {
-      clientId: 'clientId3',
-      clientSecret: 'clientSecret3',
-      ensureResources: true
-    }
-  },
-  adyen: {
-    adyenMerchantAccount1: {
-      apiKey: 'apiKey',
-      clientKey: 'clientKey'
-    },
-    adyenMerchantAccount2: {
-      apiKey: 'apiKey2',
-      clientKey: 'clientKey2'
-    },
-    adyenMerchantAccount3: {
-      apiKey: 'apiKey3',
-      clientKey: 'clientKey3'
-    }
-  },
-  logLevel: 'DEBUG',
-})
-
 const {
   execute,
 } = require('../../src/paymentHandler/klarna-make-payment.handler')
