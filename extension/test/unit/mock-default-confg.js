aceabd2628914f3a770ffe5786d8192ef9ed94ae
Lam Tran <lam.tran@commercetools.de>
2021-01-28 14:25:32 +0100

    Add unit test for multitenancy project selection (#534)
    Co-authored-by: aoz <bilmuhahmet@gmail.com>

---
+++ extension/test/unit/mock-default-confg.js
@@ -0,0 +1,34 @@
+process.env.ADYEN_INTEGRATION_CONFIG = JSON.stringify({
+  commercetools: {
+    ctpProjectKey1: {
+      clientId: 'clientId',
+      clientSecret: 'clientSecret',
+      ensureResources: true,
+    },
+    ctpProjectKey2: {
+      clientId: 'clientId2',
+      clientSecret: 'clientSecret2',
+      ensureResources: true,
+    },
+    ctpProjectKey3: {
+      clientId: 'clientId3',
+      clientSecret: 'clientSecret3',
+      ensureResources: true,
+    },
+  },
+  adyen: {
+    adyenMerchantAccount1: {
+      apiKey: 'apiKey',
+      clientKey: 'clientKey',
+    },
+    adyenMerchantAccount2: {
+      apiKey: 'apiKey2',
+      clientKey: 'clientKey2',
+    },
+    adyenMerchantAccount3: {
+      apiKey: 'apiKey3',
+      clientKey: 'clientKey3',
+    },
+  },
+  logLevel: 'DEBUG',
+})
