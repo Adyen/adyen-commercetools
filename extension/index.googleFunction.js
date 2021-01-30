336ab887912e7893b6d9938461d995ee448830af
King-Hin Leung <kinghin.leung@gmail.com>
2021-01-30 15:50:15 +0100

    Adopt payment-handler in extension module

--- extension/index.googleFunction.js
+++ extension/index.googleFunction.js
@@ -6,17 +6,31 @@
  *
  * Entry point: extensionTrigger
  */
+const paymentHandler = require('./src/paymentHandler/payment-handler')
+const utils = require('./src/utils')
 
-const paymentController = require('./src/api/payment/payment.controller')
-const httpUtils = require('./src/utils')
-
-// Google Cloud Function works with express framework. Therefore, we can simply extract
-// out the HTTP request body as follows
-httpUtils.collectRequestData = (request) => {
-  return JSON.stringify(request.body)
-}
-exports.httpUtils = httpUtils
+const logger = utils.getLogger()
 
 exports.extensionTrigger = async (request, response) => {
-  await paymentController.processRequest(request, response)
+  const { obj } = request.body.resource
+  if (!obj) {
+    const noPaymentError = new Error('No payment object is found.')
+    logger.error(
+      {
+        err: noPaymentError,
+      },
+      'Unexpected error when processing event'
+    )
+    throw noPaymentError
+  }
+  const paymentResult = await paymentHandler.handlePayment(obj)
+  if (paymentResult.success) {
+    response.status(200).send({
+      data: paymentResult.data,
+    })
+  } else {
+    response.status(400).send({
+      data: paymentResult.data,
+    })
+  }
 }
