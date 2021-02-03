c1133d5398e230e419c14ea40d6c6c1fedbf56a7
King-Hin Leung <kinghin.leung@gmail.com>
2021-02-03 16:02:36 +0100

    Add unit test for common.js to increase coverage

---
+++ notification/test/unit/commons.spec.js
@@ -0,0 +1,64 @@
+const chai = require('chai')
+
+const { getNotificationListForTracking } = require('../../src/utils/commons')
+
+const { expect } = chai
+
+describe('notification module', () => {
+  it('test', async () => {
+    const notifications = [
+      {
+        NotificationRequestItem: {
+          amount: {
+            currency: 'EUR',
+            value: 10100,
+          },
+          eventCode: 'AUTHORISATION',
+          eventDate: '2019-01-30T18:16:22+01:00',
+          merchantAccountCode: 'CommercetoolsGmbHDE775',
+          merchantReference: '8313842560770001',
+          operations: ['CANCEL', 'CAPTURE', 'REFUND'],
+          paymentMethod: 'visa',
+          pspReference: 'test_AUTHORISATION_1',
+          success: 'true',
+        },
+      },
+      {
+        NotificationRequestItem: {
+          amount: {
+            currency: 'USD',
+            value: 10100,
+          },
+          eventCode: 'AUTHORISATION',
+          eventDate: '2019-02-01T18:16:22+01:00',
+          merchantAccountCode: 'CommercetoolsGmbHDE775',
+          merchantReference: '8313842560770001',
+          operations: ['CANCEL', 'CAPTURE', 'REFUND'],
+          paymentMethod: 'visa',
+          pspReference: 'test_AUTHORISATION_2',
+          success: 'true',
+        },
+      },
+    ]
+    const actualNotificationResult = await getNotificationListForTracking(
+      notifications
+    )
+    const expectedNotificationResult = [
+      {
+        eventCode: 'AUTHORISATION',
+        eventDate: '2019-01-30T18:16:22+01:00',
+        pspReference: 'test_AUTHORISATION_1',
+        success: 'true',
+      },
+      {
+        eventCode: 'AUTHORISATION',
+        eventDate: '2019-02-01T18:16:22+01:00',
+        pspReference: 'test_AUTHORISATION_2',
+        success: 'true',
+      },
+    ]
+
+    // assert notification for tracking
+    expect(actualNotificationResult).to.deep.equal(expectedNotificationResult)
+  })
+})
