c54b9dbe747b44fb098512e60ccd04e16df7f9f1
King-Hin Leung <kinghin.leung@gmail.com>
2021-02-05 11:08:45 +0100

    Avoid throwing exception to CTP from extension module

--- extension/index.googleFunction.js
+++ extension/index.googleFunction.js
@@ -11,24 +11,29 @@ const logger = utils.getLogger()
 
 exports.extensionTrigger = async (request, response) => {
   const { obj } = request.body.resource
+
   if (!obj) {
-    const noPaymentError = new Error('No payment object is found.')
-    logger.error(
-      {
-        err: noPaymentError,
-      },
-      'Unexpected error when processing event'
-    )
-    throw noPaymentError
-  }
-  const paymentResult = await paymentHandler.handlePayment(obj)
-  if (paymentResult.success) {
-    response.status(200).send({
-      data: paymentResult.data,
-    })
+    const errorMessage = 'No payment object is found.'
+    const noPaymentErrorJson = {
+      errors: [
+        {
+          code: 'InvalidInput',
+          message: errorMessage,
+        },
+      ],
+    }
+    logger.error(errorMessage)
+    response.status(400).send(noPaymentErrorJson)
   } else {
-    response.status(400).send({
-      data: paymentResult.data,
-    })
+    const paymentResult = await paymentHandler.handlePayment(obj)
+    if (paymentResult.success) {
+      response.status(200).send({
+        data: paymentResult.data,
+      })
+    } else {
+      response.status(400).send({
+        data: paymentResult.data,
+      })
+    }
   }
 }
