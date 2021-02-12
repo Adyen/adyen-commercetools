3772106d10cc2c579963e3c9fb83ae480bb6e4b5
aoz <bilmuhahmet@gmail.com>
2021-02-12 13:02:12 +0100

    Update AWS lambda function.

--- notification/src/lambda.js
+++ notification/src/lambda.js
@@ -1,11 +1,7 @@
 const handler = require('./handler/notification/notification.handler')
-const config = require('./config/config')
 const logger = require('./utils/logger').getLogger()
-const setup = require('./config/init/ensure-interface-interaction-custom-type')
-
 const { getNotificationForTracking } = require('./utils/commons')
-
-let initialised = false
+const { getCtpProjectConfig, getAdyenConfig } = require('./utils/parser')
 
 exports.handler = async function (event) {
   const { notificationItems } = event
@@ -20,37 +16,28 @@ exports.handler = async function (event) {
     )
     throw error
   }
-  for (const notification of notificationItems) {
-    try {
-      const commercetoolsProjectKey =
-        notification.NotificationRequestItem.additionalData[
-          'metadata.commercetoolsProjectKey'
-        ]
-      const adyenMerchantAccount =
-        notification.NotificationRequestItem.merchantAccountCode
-      const ctpProjectConfig = config.getCtpConfig(commercetoolsProjectKey)
-      const adyenConfig = config.getAdyenConfig(adyenMerchantAccount)
+  try {
+    for (const notification of notificationItems) {
+      const ctpProjectConfig = getCtpProjectConfig(notification)
+      const adyenConfig = getAdyenConfig(notification)
 
-      if (!initialised) {
-        await setup.ensureInterfaceInteractionCustomType(ctpProjectConfig)
-        initialised = true
-      }
       await handler.processNotification(
         notification,
         adyenConfig.enableHmacSignature,
         ctpProjectConfig
       )
-    } catch (e) {
-      logger.error(
-        {
-          notification: getNotificationForTracking(notification),
-          err: e,
-        },
-        'Unexpected error when processing event'
-      )
-      throw e
     }
   }
+  catch (err) {
+    logger.error(
+      { notification: getNotificationForTracking(notificationItems), err },
+      'Unexpected exception occurred.'
+    )
+    if (err.isRecoverable) {
+      throw err
+    }
+  }
+
   return {
     notificationResponse: '[accepted]',
   }
