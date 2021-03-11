579913c83f62669e3efe19751d1dccb07c4ad827
King-Hin Leung <kinghin.leung@gmail.com>
2021-03-11 15:16:49 +0100

    Revert "Add happy path for payment authentication integration test"
    This reverts commit e36fd66a8adaa9f3688967c4745740415523c771.

--- extension/test/integration/make-unauthorized-payment.handler.spec.js
+++ extension/test/integration/make-unauthorized-payment.handler.spec.js
@@ -4,9 +4,8 @@ const ctpClientBuilder = require('../../src/ctp')
 const config = require('../../src/config/config')
 const constants = require('../../src/config/constants')
 const iTSetUp = require('./integration-test-set-up')
-const setup = require('../../src/setup')
 
-describe(':: Test case for make-payment request with authentication process enabled ::', () => {
+describe('::unauthorized make-payment request use case::', () => {
   const [commercetoolsProjectKey] = config.getAllCtpProjectKeys()
   const [
     adyenMerchantAccount1,
@@ -17,8 +16,7 @@ describe(':: Test case for make-payment request with authentication process enab
   let originalCtpConfig
   let ctpConfig = config.getCtpConfig(commercetoolsProjectKey)
 
-  beforeEach(async () => {
-    originalCtpConfig = ctpConfig
+  before(async () => {
     ctpClient = ctpClientBuilder.get(ctpConfig)
     await iTSetUp.cleanupCtpResources(ctpClient)
     await iTSetUp.initServerAndExtension({
@@ -32,6 +30,9 @@ describe(':: Test case for make-payment request with authentication process enab
       password: 'open sesame',
     })
   })
+  beforeEach(async () => {
+    originalCtpConfig = ctpConfig
+  })
 
   afterEach(async () => {
     ctpConfig = originalCtpConfig
@@ -42,51 +43,21 @@ describe(':: Test case for make-payment request with authentication process enab
   })
 
   it(
-      'given a single commercetools project with enabled authorization for extenstion module and payments ' +
+    'given a single commercetools project with enabled authorization for extenstion module and payments ' +
       'when sending payments with incorrect authorization header, ' +
       'then it should fail to authenticate ',
-      async () => {
-        try {
-          await Promise.all([
-            makePayment({
-              reference: 'paymentFromMerchant1',
-              adyenMerchantAccount: adyenMerchantAccount1,
-            }),
-            makePayment({
-              reference: 'paymentFromMerchant2',
-              adyenMerchantAccount: adyenMerchantAccount2,
-            }),
-          ])
-          expect.fail('This test should throw an error, but it did not')
-        } catch (e) {
-          expect(e.statusCode).to.equal(400)
-          expect(e.message).to.equal('The request is unauthorized.')
-        }
-      }
-  )
-
-  it(
-      'given a single commercetools project with enabled authorization for extenstion module and payments ' +
-      'when sending payments with correct authorization header, ' +
-      'then it should success to make payment ',
-      async () => {
-        await setup.setupExtensionResources()
-        const response = await Promise.all([
-          makePayment({
-            reference: 'paymentFromMerchant3',
-            adyenMerchantAccount: adyenMerchantAccount1,
-          }),
-          makePayment({
-            reference: 'paymentFromMerchant4',
-            adyenMerchantAccount: adyenMerchantAccount2,
-          }),
-        ])
-        expect(response.length).to.equal(2)
-        expect(response[0].statusCode).to.equal(201)
-        expect(response[1].statusCode).to.equal(201)
-        expect(response[0].body.key).to.equal('paymentFromMerchant3')
-        expect(response[1].body.key).to.equal('paymentFromMerchant4')
-      }
+    async () => {
+      await Promise.all([
+        makePayment({
+          reference: 'paymentFromMerchant1',
+          adyenMerchantAccount: adyenMerchantAccount1,
+        }),
+        makePayment({
+          reference: 'paymentFromMerchant2',
+          adyenMerchantAccount: adyenMerchantAccount2,
+        }),
+      ])
+    }
   )
 
   async function makePayment({ reference, adyenMerchantAccount }) {
@@ -125,10 +96,12 @@ describe(':: Test case for make-payment request with authentication process enab
         },
       },
     }
-    const response = await ctpClient.create(
-        ctpClient.builder.payments,
-        paymentDraft
-    )
-    return response
+    try {
+      await ctpClient.create(ctpClient.builder.payments, paymentDraft)
+      expect.fail('This test should throw an error, but it did not')
+    } catch (e) {
+      expect(e.statusCode).to.equal(400)
+      expect(e.message).to.equal('The request is unauthorized.')
+    }
   }
 })
