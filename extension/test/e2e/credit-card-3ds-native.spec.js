82b2d7a6022747668b4360cfde8c3a220ee4f19d
King-Hin Leung <kinghin.leung@gmail.com>
2021-06-14 12:24:14 +0200

    Extend the browser timeout limit for e2e test

--- extension/test/e2e/credit-card-3ds-native.spec.js
+++ extension/test/e2e/credit-card-3ds-native.spec.js
@@ -161,7 +161,7 @@ describe('::creditCardPayment3dsNative::', () => {
       makePaymentResponse
     )
 
-    await browserTab.waitForTimeout(3000)
+    await browserTab.waitForTimeout(10_000)
 
     // Submit additional details
     const creditCardNativePage = new CreditCardNativePage(browserTab, baseUrl)
