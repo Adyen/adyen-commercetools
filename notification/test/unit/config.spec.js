b3ee93bd5b59eb314637b2e6655e1d1cce908da3
King-Hin Leung <kinghin.leung@gmail.com>
2021-09-08 17:39:03 +0200

    Add unit test for String value in removeSensitiveData attribute

--- notification/test/unit/config.spec.js
+++ notification/test/unit/config.spec.js
@@ -85,4 +85,56 @@ describe('::config::', () => {
       expect(config.getModuleConfig()?.removeSensitiveData).to.eql(true)
     }
   )
+
+  it(
+    'when removeSensitiveData is set as string false in config.js, ' +
+      'it should load as false value in module config',
+    () => {
+      process.env.ADYEN_INTEGRATION_CONFIG = JSON.stringify({
+        commercetools: {
+          ctpProjectKey1: {
+            clientId: 'clientId',
+            clientSecret: 'clientSecret',
+            apiUrl: 'host',
+            authUrl: 'authUrl',
+          },
+        },
+        adyen: {
+          adyenMerchantAccount1: {
+            enableHmacSignature: 'false',
+          },
+        },
+        logLevel: 'DEBUG',
+        removeSensitiveData: 'false',
+      })
+      const config = requireUncached('../../src/config/config')
+      expect(config.getModuleConfig()?.removeSensitiveData).to.eql(false)
+    }
+  )
+
+  it(
+    'when removeSensitiveData is set as string true in config.js, ' +
+      'it should load as true value in module config',
+    () => {
+      process.env.ADYEN_INTEGRATION_CONFIG = JSON.stringify({
+        commercetools: {
+          ctpProjectKey1: {
+            clientId: 'clientId',
+            clientSecret: 'clientSecret',
+            apiUrl: 'host',
+            authUrl: 'authUrl',
+          },
+        },
+        adyen: {
+          adyenMerchantAccount1: {
+            enableHmacSignature: 'false',
+          },
+        },
+        logLevel: 'DEBUG',
+        removeSensitiveData: 'true',
+      })
+      const config = requireUncached('../../src/config/config')
+      expect(config.getModuleConfig()?.removeSensitiveData).to.eql(true)
+    }
+  )
 })
