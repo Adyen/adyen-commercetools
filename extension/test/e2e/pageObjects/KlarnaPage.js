d5a6d679d1e83a21ae9774d69f13506631be41a2
King-Hin Leung <kinghin.leung@gmail.com>
2021-09-28 15:01:42 +0200

    Fix style

--- extension/test/e2e/pageObjects/KlarnaPage.js
+++ extension/test/e2e/pageObjects/KlarnaPage.js
@@ -8,10 +8,8 @@ module.exports = class KlarnaPage {
     const confirmationFrame = this.page
       .frames()
       .find((f) => f.name() === 'klarna-hpp-instance-fullscreen')
-    await confirmationFrame.waitForSelector(
-      '#iban'
-    )
-    await confirmationFrame.type('#iban','DE11520513735120710131') // Testing IBAN provided by Klarna
+    await confirmationFrame.waitForSelector('#iban')
+    await confirmationFrame.type('#iban', 'DE11520513735120710131') // Testing IBAN provided by Klarna
     await confirmationFrame.click('#aligned-content__button__0')
     await this.page.waitForTimeout(500)
     return confirmationFrame.click('#aligned-content__button__0')
