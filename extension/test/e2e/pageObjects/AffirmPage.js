1552c81499c5037091d812d0d3235c35df27efe6
King-Hin Leung <kinghin.leung@gmail.com>
2021-10-05 19:35:23 +0200

    Refactor AffirmPage.js

--- extension/test/e2e/pageObjects/AffirmPage.js
+++ extension/test/e2e/pageObjects/AffirmPage.js
@@ -4,26 +4,32 @@ module.exports = class AffirmPage {
   }
 
   async finishAffirmPayment() {
-    // Input phone number and proceed
+    await this.inputPhoneNumberAndSubmitButton()
+    await this.inputPIIN()
+    await this.clickAutoPayToggleAndProceed()
+    return this.enterConfirmationPageAndClickConfirmButton()
+  }
+
+  async inputPhoneNumberAndSubmitButton() {
     await this.page.type('[data-testid="phone-number-field"]', '212-220-3809')
     await this.page.click('[data-testid="submit-button"]')
     await this.page.waitForSelector('[aria-label="PIN"]')
+  }
 
-    // Input PIN code
+  async inputPIIN() {
     await this.page.type('[aria-label="PIN"]', '1234')
-
-    // Enter the 2nd page and press continue button
     await this.page.waitForSelector('#confirm-submit')
+  }
+
+  async clickAutoPayToggleAndProceed() {
     const autoPayToggle = await this.page.$('#autopay-toggle')
     await this.page.evaluate((cb) => cb.click(), autoPayToggle)
-
-    // Wait for the page refreshes after toggling the autopay
-    await this.page.waitForTimeout(1_000)
-
+    await this.page.waitForTimeout(1_000) // Wait for the page refreshes after toggling the autopay
     await this.page.click('#confirm-submit')
     await this.page.waitForSelector('#confirm-disclosure-checkbox')
+  }
 
-    // Enter confirmation page and press confirm button
+  async enterConfirmationPageAndClickConfirmButton() {
     const confirmCheckbox = await this.page.$('#confirm-disclosure-checkbox')
     await this.page.evaluate((cb) => cb.click(), confirmCheckbox)
     return this.page.click('[data-test="confirm-loan-submit"]')
