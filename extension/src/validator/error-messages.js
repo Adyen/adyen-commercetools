e01b5fc65a09a6ac5794d1099bc22bf5e2669e34
aoz <bilmuhahmet@gmail.com>
2021-11-04 18:57:01 +0100

    small improvements.

--- extension/src/validator/error-messages.js
+++ extension/src/validator/error-messages.js
@@ -3,7 +3,6 @@ module.exports = {
     'getPaymentMethodsRequest does not contain valid JSON.',
   MAKE_PAYMENT_REQUEST_INVALID_JSON:
     'makePaymentRequest does not contain valid JSON.',
-  // eslint-disable-next-line max-len
   SUBMIT_ADDITIONAL_PAYMENT_DETAILS_REQUEST_INVALID_JSON:
     'submitAdditionalPaymentDetailsRequest does not contain valid JSON.',
   AMOUNT_PLANNED_NOT_SAME:
