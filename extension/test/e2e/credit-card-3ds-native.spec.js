7849b23e4883928ce791946bb960c1eef5d7f08f
lojzatran <lam.tran@commercetools.com>
2021-11-22 19:27:41 +0100

    Refactor the test

--- extension/test/e2e/credit-card-3ds-native.spec.js
+++ extension/test/e2e/credit-card-3ds-native.spec.js
@@ -21,14 +21,6 @@ describe('::creditCardPayment3dsNative::', () => {
   const adyenMerchantAccount = config.getAllAdyenMerchantAccounts()[0]
   const ctpProjectKey = config.getAllCtpProjectKeys()[0]
 
-  // See more: https://docs.adyen.com/development-resources/test-cards/test-card-numbers
-  const creditCards = [
-    {
-      name: 'Visa',
-      creditCardNumber: '4917 6100 0000 0000',
-    },
-  ]
-
   beforeEach(async () => {
     routes['/make-payment-form'] = async (request, response) => {
       serveFile(
@@ -66,62 +58,57 @@ describe('::creditCardPayment3dsNative::', () => {
     await browser.close()
   })
 
-  creditCards.forEach(
-    ({
-      name,
-      creditCardNumber,
-      creditCardDate = '03/30',
-      creditCardCvc = '737',
-    }) => {
-      // eslint-disable-next-line no-template-curly-in-string
-      it(
-        `when credit card issuer is ${name} and credit card number is ${creditCardNumber}, ` +
-          'then it should successfully finish the payment with 3DS native authentication flow',
-        async () => {
-          const baseUrl = config.getModuleConfig().apiExtensionBaseUrl
-          const clientKey =
-            config.getAdyenConfig(adyenMerchantAccount).clientKey
-          let paymentAfterAuthentication
-          try {
-            const payment = await createPayment(
-              ctpClient,
-              adyenMerchantAccount,
-              ctpProjectKey
-            )
-            logger.debug(
-              'credit-card-3ds-native::payment:',
-              JSON.stringify(payment)
-            )
-            const browserTab = await browser.newPage()
+  it(
+    `when credit card issuer is Visa and credit card number is 4917 6100 0000 0000, ` +
+      'then it should successfully finish the payment with 3DS native authentication flow',
+    async () => {
+      // See more: https://docs.adyen.com/development-resources/test-cards/test-card-numbers
+      const creditCardNumber = '4917 6100 0000 0000'
+      const creditCardDate = '03/30'
+      const creditCardCvc = '737'
 
-            const paymentAfterMakePayment = await makePayment({
-              browserTab,
-              baseUrl,
-              creditCardNumber,
-              creditCardDate,
-              creditCardCvc,
-              payment,
-              clientKey,
-            })
-            logger.debug(
-              'credit-card-3ds-native::paymentAfterMakePayment:',
-              JSON.stringify(paymentAfterMakePayment)
-            )
-            paymentAfterAuthentication = await performChallengeFlow({
-              payment: paymentAfterMakePayment,
-              browserTab,
-              baseUrl,
-            })
-            logger.debug(
-              'credit-card-3ds-native::paymentAfterAuthentication:',
-              JSON.stringify(paymentAfterAuthentication)
-            )
-          } catch (err) {
-            logger.error('credit-card-3ds-native::errors', JSON.stringify(err))
-          }
-          assertPayment(paymentAfterAuthentication)
-        }
-      )
+      const baseUrl = config.getModuleConfig().apiExtensionBaseUrl
+      const clientKey =
+        config.getAdyenConfig(adyenMerchantAccount).clientKey
+      let paymentAfterAuthentication
+      try {
+        const payment = await createPayment(
+          ctpClient,
+          adyenMerchantAccount,
+          ctpProjectKey
+        )
+        logger.debug(
+          'credit-card-3ds-native::payment:',
+          JSON.stringify(payment)
+        )
+        const browserTab = await browser.newPage()
+
+        const paymentAfterMakePayment = await makePayment({
+          browserTab,
+          baseUrl,
+          creditCardNumber,
+          creditCardDate,
+          creditCardCvc,
+          payment,
+          clientKey,
+        })
+        logger.debug(
+          'credit-card-3ds-native::paymentAfterMakePayment:',
+          JSON.stringify(paymentAfterMakePayment)
+        )
+        paymentAfterAuthentication = await performChallengeFlow({
+          payment: paymentAfterMakePayment,
+          browserTab,
+          baseUrl,
+        })
+        logger.debug(
+          'credit-card-3ds-native::paymentAfterAuthentication:',
+          JSON.stringify(paymentAfterAuthentication)
+        )
+      } catch (err) {
+        logger.error('credit-card-3ds-native::errors', JSON.stringify(err))
+      }
+      assertPayment(paymentAfterAuthentication)
     }
   )
 
@@ -178,7 +165,7 @@ describe('::creditCardPayment3dsNative::', () => {
       makePaymentResponse
     )
 
-    await this.page.waitForTimeout(5_000)
+    await browserTab.waitForTimeout(5_000)
 
     // Submit additional details
     const creditCardNativePage = new CreditCardNativePage(browserTab, baseUrl)
