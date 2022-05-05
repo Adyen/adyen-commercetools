f0382382c009848458b241cef502bdffa1ef691c
lojzatran <lam.tran@commercetools.com>
2022-05-05 10:47:43 +0200

    Update notification to parse aws url correctly

--- notification/index.lambda.js
+++ notification/index.lambda.js
@@ -21,7 +21,7 @@ exports.handler = async (event) => {
   }
   try {
     for (const notification of notificationItems) {
-      const ctpProjectConfig = getCtpProjectConfig(notification)
+      const ctpProjectConfig = getCtpProjectConfig(notification, event.path)
       const adyenConfig = getAdyenConfig(notification)
 
       await handler.processNotification(
