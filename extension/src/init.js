d009626e76500636dc77caccde535df8e8a41f7b
praveenkumarct <praveen.kumar@commercetools.com>
2022-05-27 09:02:16 +0200

    Fix syntax issues in the init file.

--- extension/src/init.js
+++ extension/src/init.js
@@ -1,11 +1,14 @@
 import { setupServer } from './server.js'
-import logger from './utils.js'
+import utils from './utils.js'
 import config from './config/config.js'
 
 const port = config.getModuleConfig().port || 8080
 
+const server = setupServer()
+const logger = utils.getLogger()
+
 if (config.getModuleConfig().keepAliveTimeout !== undefined)
-  setupServer.keepAliveTimeout = config.getModuleConfig().keepAliveTimeout
-setupServer.listen(port, async () => {
+  server.keepAliveTimeout = config.getModuleConfig().keepAliveTimeout
+server.listen(port, async () => {
   logger.info(`Extension module is running at http://localhost:${port}`)
 })
