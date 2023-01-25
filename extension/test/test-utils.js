93d33ebe0ce1473d35e514e0695c893d3e72f167
King-Hin Leung <>
2023-01-25 18:46:43 +0100

    Debug notification URL

--- extension/test/test-utils.js
+++ extension/test/test-utils.js
@@ -29,7 +29,7 @@ async function startIT() {
   if (process.env.CI) {
     // this part used only on github actions (CI)
     await setupExtensionResources(process.env.CI_EXTENSION_BASE_URL)
-    console.log('notification URL')
+    console.log('notification URL 2')
     console.log(process.env.CI_NOTIFICATION_URL)
     await ensureAdyenWebhookForAllAdyenAccounts(process.env.CI_NOTIFICATION_URL)
     // e2e requires this for static forms
