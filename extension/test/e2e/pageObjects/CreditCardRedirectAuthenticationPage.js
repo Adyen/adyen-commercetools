83ff06a8cb6805319b44baa3ab09ea72f3cb9ef9
King-Hin Leung <>
2023-01-26 09:53:54 +0100

    Debug e2e test failure in CI

--- extension/test/e2e/pageObjects/CreditCardRedirectAuthenticationPage.js
+++ extension/test/e2e/pageObjects/CreditCardRedirectAuthenticationPage.js
@@ -4,17 +4,14 @@ export default class CreditCardRedirectAuthenticationPage {
   }
 
   async doPaymentAuthentication() {
-    await this.page.waitForTimeout(5_000)
+    await this.page.waitForTimeout(2_000)
     await this.page.waitForSelector('#username')
     await this.page.type('#username', 'user')
     await this.page.type('#password', 'password')
 
-    await Promise.all([
-      this.page.click('.paySubmit'),
-      this.page.waitForSelector('#redirect-response'),
-    ])
+    this.page.click('.paySubmit'),
 
-    await this.page.waitForTimeout(2_000)
+    await this.page.waitForTimeout(5_000) // wait for redirect response
     const sessionIdEle = await this.page.$('#sessionId')
     const redirectResultEle = await this.page.$('#redirectResult')
     const sessionId = await this.page.evaluate(
