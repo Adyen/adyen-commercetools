940b86abac9910f639493e8624484d1ebdd01b00
King-Hin Leung <>
2023-02-16 16:26:11 +0100

    Revert "Disable the explicit handling on recurringDetailsReference in notification module"
    This reverts commit 35a4d57f23df5cb21054030f5f921f6449058319.

--- extension/test/e2e/credit-card-disable-stored-payment.spec.js
+++ extension/test/e2e/credit-card-disable-stored-payment.spec.js
@@ -257,7 +257,6 @@ describe('::creditCardPayment::disable-stored-payment::', () => {
     createSessionRequestJson.shopperReference = 'shopperReference'
     createSessionRequestJson.shopperInteraction = 'Ecommerce'
     createSessionRequestJson.recurringProcessingModel = recurringProcessingModel
-    createSessionRequestJson.removeSensitiveData = false
     createSessionRequest = JSON.stringify(createSessionRequestJson)
     let payment = null
     payment = await createPaymentSession(
