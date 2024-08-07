4b091dcff55d64ea77b5b5d2c41dc981361cbfa0
Marcin Krysinski <marcin.krysinski@codequest.com>
2024-08-07 08:59:35 +0200

    refactor: pass logger with more context

--- notification/src/handler/notification/notification.handler.js
+++ notification/src/handler/notification/notification.handler.js
@@ -54,7 +54,7 @@ async function processNotification({
   )
   // if payment doesn't exist => log error and return
   if (!payment) {
-    logger.error(
+    ctpLogger.error(
       `Payment with merchantReference: ${merchantReference} was not found`,
     )
 
@@ -65,7 +65,7 @@ async function processNotification({
     payment.custom.fields.makePaymentResponse ||
     payment.custom.fields.createSessionResponse
   ) {
-    await updatePaymentWithRepeater(payment, notification, ctpClient, logger)
+    await updatePaymentWithRepeater(payment, notification, ctpClient, ctpLogger)
 
     return
   }
@@ -78,7 +78,7 @@ async function processNotification({
   )
 
   if (!payment) {
-    logger.error(
+    ctpLogger.error(
       `Payment with merchantReference: ${merchantReference} was not found`,
     )
 
@@ -89,7 +89,7 @@ async function processNotification({
   // if pspReference or originalReference from webhook are the same as the payment key => standard update
   // if not => add a transaction with the message to the payment
   // so the merchant could see that the webhook wasn't correct
-  await updatePaymentWithRepeater(payment, notification, ctpClient, logger)
+  await updatePaymentWithRepeater(payment, notification, ctpClient, ctpLogger)
 }
 
 function sleep(ms) {
